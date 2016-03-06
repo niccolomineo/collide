de4c7ee03fa1974a267330015d722da9bfb3bc7d
Niccolò Mineo <niccolomineo@MBPro.local>
2016-03-06 19:02:40 +0100

    Overhaul to accept a txt file as the map's db

--- js/collide.js
+++ js/collide.js
@@ -1,80 +1,95 @@
 /**
-* Collide.js - A simple collision detection experiment
-* coded with ♥ by Niccolò Mineo
-* www.niccolomineo.com
-* © 2016
-*/
+ * Collide.js - A simple collision detection experiment
+ * coded with ♥ by Niccolò Mineo
+ * www.niccolomineo.com
+ * © 2016
+ */
 
 /**
-* INSTRUCTIONS TO BUILD THE MAP
-* > The array must be a rectangle or a square
-* > In the array, use:
-* * - X to put walls and delimit your space
-* * - O to mark user start position
-* * - Space to mark walkable space
-* > Adjust mapWidth variable to the number of columns in array
-*/
+ * INSTRUCTIONS TO BUILD THE MAP
+ * Edit example in ./data/map.txt
+ * > The map must be a rectangle or a square
+ * > Use:
+ * * - X to put walls and delimit your space
+ * * - O to mark user start position
+ * * - a space to mark walkable floor space
+ */
 
-var O = '\u25B8',
-X = '8',
-map = [
-X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,
-X, , , ,X,X, , , ,X,X, , , ,X,
-X, , , , , , , , , , , , , ,X,
-X, , , , , , ,O, , , , , , ,X,
-X, , , ,X,X, , , ,X,X, , , ,X,
-X,X,X,X,X,X,X,X,X,X,X,X,X,X,X
-],
-mapWidth = 15,
-floor = '\u00A0',
-keyDownText = document.getElementById('pressedKeys'),
-mapText = document.getElementById('map'),
-moveToPos,
-userPos = map.indexOf(O),
-e = e || window.event,
-mapWidthRegEx = '(.{' + mapWidth + '})',
-mapWidthRegExInstance = new RegExp(mapWidthRegEx, "g");
+var fileName = 'map';
 
-for (i=0; i < map.length; i++) {
-  map[i] === undefined ? map[i] = floor : map[i];
-}
+/**
+ * MAP ENGINE
+ */
 
-mapText.textContent = map.join('').replace(mapWidthRegExInstance,'$1\n'); // flattens map, adds new line every 15 chars and draws the whole thing
+(function(fileName) {
+  var file = new XMLHttpRequest();
+  file.open('GET', './data/' + fileName + '.txt', true);
+  file.onreadystatechange = function() {
+    if (file.readyState === 4) {
+      if (file.status === 200 || file.status == 0) {
 
-document.onkeydown = function(e) {
+        var O = '\u25B8',
+          X = '8',
+          floor = '\u00A0',
+          responseText = file.responseText,
+          map = responseText.replace(/\n/g, '').split(','),
+          mapWidth = responseText.replace(/\,/g, '').split('\n')[0].length,
+          mapWidthRegEx = '(.{' + mapWidth + '})',
+          mapWidthRegExInstance = new RegExp(mapWidthRegEx, "g"),
+          userPos = map.indexOf('O'),
+          moveToPos,
+          keyDownText = document.getElementById('pressedKeys'),
+          mapText = document.getElementById('map'),
+          e = e || window.event;
 
-  switch (e.keyCode) {
-    case 37:
-    moveToPos = map.indexOf(O) - 1;
-    O = '\u25C2';
-    keyDownText.textContent = 'left';
-    break;
-    case 38:
-    moveToPos = map.indexOf(O) - mapWidth;
-    O = '\u25B4';
-    keyDownText.textContent = 'up';
-    break;
-    case 39:
-    moveToPos = map.indexOf(O) + 1;
-    O = '\u25B8';
-    keyDownText.textContent = 'right';
-    break;
-    case 40:
-    moveToPos = map.indexOf(O) + mapWidth;
-    O = '\u25BE';
-    keyDownText.textContent = 'down';
-    break;
+        for (i = 0; i < map.length; i++) {
+          map[i] === ' ' ? map[i] = floor : map[i];
+          map[i] === 'X' ? map[i] = X : map[i];
+          map[i] === 'O' ? map[i] = O : map[i];
+        }
+
+        mapText.textContent = map.join('').replace(mapWidthRegExInstance, '$1\n'); // flattens map, adds new line every 15 chars and draws the whole thing
+
+        document.onkeydown = function(e) {
+
+          switch (e.keyCode) {
+            case 37:
+              moveToPos = map.indexOf(O) - 1;
+              O = '\u25C2';
+              keyDownText.textContent = 'left';
+              break;
+            case 38:
+              moveToPos = map.indexOf(O) - mapWidth;
+              O = '\u25B4';
+              keyDownText.textContent = 'up';
+              break;
+            case 39:
+              moveToPos = map.indexOf(O) + 1;
+              O = '\u25B8';
+              keyDownText.textContent = 'right';
+              break;
+            case 40:
+              moveToPos = map.indexOf(O) + mapWidth;
+              O = '\u25BE';
+              keyDownText.textContent = 'down';
+              break;
+          }
+
+          map.splice(userPos, 1, floor); // remove user from previous position
+
+          if (map.join('').charAt(moveToPos) != X) {
+            map.splice(moveToPos, 1, O); // move user to new position
+            userPos = moveToPos;
+          } else {
+            map.splice(userPos, 1, O); // keep user in the same position
+          }
+
+          mapText.textContent = map.join('').replace(mapWidthRegExInstance, "$1\n");
+
+        }
+
+      }
+    }
   }
-
-  map.splice(userPos, 1, floor); // remove user from previous position
-
-  if (map.join('').charAt(moveToPos) != X) {
-    map.splice(moveToPos, 1, O); // move user to new position
-    userPos = moveToPos;
-  } else {
-    map.splice(userPos, 1, O); // keep user in the same position
-  }
-
-  mapText.textContent = map.join('').replace(mapWidthRegExInstance,"$1\n");
-
-}
+  file.send(null);
+})(fileName);
